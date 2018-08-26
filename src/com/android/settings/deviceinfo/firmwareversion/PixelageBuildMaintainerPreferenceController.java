5700bd93ad96afee05a8e44e68f6eae98df7901a
LuK1337 <priv.luk@gmail.com>
2018-08-26 15:00:21 +0200

    Settings: Add Pixelage entries into device info
    Change-Id: I4acf834442712954f8e9db15370bffafd4547174

---
+++ src/com/android/settings/deviceinfo/firmwareversion/PixelageBuildMaintainerPreferenceController.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2019-2024 The Evolution X Project
+ * Copyright (C) 2024 Project Pixelage
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.settings.deviceinfo.firmwareversion;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
+import android.text.TextUtils;
+import android.util.Log;
+
+import androidx.preference.Preference;
+
+import com.android.settings.R;
+import com.android.settings.core.BasePreferenceController;
+
+public class PixelageBuildMaintainerPreferenceController extends BasePreferenceController {
+
+    private static final String TAG = "PixelageBuildMaintainerCtrl";
+
+    private String mDeviceMaintainer;
+
+    public PixelageBuildMaintainerPreferenceController(Context context, String key) {
+        super(context, key);
+        mDeviceMaintainer = mContext.getResources().getString(R.string.build_maintainer_summary);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        if (mDeviceMaintainer.equalsIgnoreCase("UNKNOWN")) {
+            return UNSUPPORTED_ON_DEVICE;
+        }
+        return AVAILABLE;
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        return mDeviceMaintainer;
+    }
+}
