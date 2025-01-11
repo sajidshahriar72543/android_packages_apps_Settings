b54ccbcdb970fc323e9cf5e5a745f4b817d5dd32
sajidshahriar72543 <sazidshahriar39@gmail.com>
2025-01-11 07:38:00 +0000

    Settings: Pull maintainer summary from ro.pixelage.maintainer prop

--- src/com/android/settings/deviceinfo/firmwareversion/PixelageBuildMaintainerPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/PixelageBuildMaintainerPreferenceController.java
@@ -7,11 +7,8 @@
 package com.android.settings.deviceinfo.firmwareversion;
 
 import android.content.Context;
-import android.content.Intent;
-import android.content.res.Resources;
-import android.net.Uri;
+import android.os.SystemProperties;
 import android.text.TextUtils;
-import android.util.Log;
 
 import androidx.preference.Preference;
 
@@ -21,24 +18,21 @@ import com.android.settings.core.BasePreferenceController;
 public class PixelageBuildMaintainerPreferenceController extends BasePreferenceController {
 
     private static final String TAG = "PixelageBuildMaintainerCtrl";
-
-    private String mDeviceMaintainer;
+    private static final String PROPERTY_MAINTAINER = "ro.pixelage.maintainer";
 
     public PixelageBuildMaintainerPreferenceController(Context context, String key) {
         super(context, key);
-        mDeviceMaintainer = mContext.getResources().getString(R.string.build_maintainer_summary);
     }
 
     @Override
     public int getAvailabilityStatus() {
-        if (mDeviceMaintainer.equalsIgnoreCase("UNKNOWN")) {
-            return UNSUPPORTED_ON_DEVICE;
-        }
         return AVAILABLE;
     }
 
     @Override
     public CharSequence getSummary() {
+        String mDeviceMaintainer = SystemProperties.get(PROPERTY_MAINTAINER,
+                this.mContext.getString(R.string.device_info_default));
         return mDeviceMaintainer;
     }
 }
